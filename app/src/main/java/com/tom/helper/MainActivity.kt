98d664b746e280001a3885fe63efdd84855eef37
Will Lo <darthhun54@gmail.com>
2020-01-27 18:25:22 +0800

    try to implement Repository & works

--- app/src/main/java/com/tom/helper/MainActivity.kt
+++ app/src/main/java/com/tom/helper/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -28,33 +29,33 @@ class MainActivity : AppCompatActivity() {
 
 
         bottomNavigation.setOnNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.fragment_profile ->{
+            when (it.itemId) {
+                R.id.fragment_profile -> {
 //                    if (!UserManager.isLoggedIn()){
 //                        fragment = FacebookBottomSheet.newInstance()
 //                        fragment.show(supportFragmentManager, "facebook_login")
 //                        false
 //                    } else {
-                        navController.navigate(R.id.action_global_profileFragment)
-                        true
-                    }
+                    navController.navigate(R.id.action_global_profileFragment)
+                    true
+                }
 
 
-                R.id.fragment_home ->{
+                R.id.fragment_home -> {
                     navController.navigate(R.id.action_global_homeFragment)
                     true
                 }
 
-                R.id.fragment_post_request ->{
+                R.id.fragment_post_request -> {
                     navController.navigate(R.id.action_global_postRequestFragment)
                     true
                 }
 
-                R.id.fragment_ranking_list ->{
+                R.id.fragment_ranking_list -> {
                     navController.navigate(R.id.rankingListFragment)
                     true
                 }
-                else ->{
+                else -> {
                     false
                 }
 
@@ -65,6 +66,39 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+
+    //handle changing the title while selecting different fragments
+
+    enum class EnumCheck {
+        HOME, POSTREQUEST, RANKINGLIST, PROFILE
+    }
+
+//    fun setLogo(forCheck: EnumCheck) {
+//
+//        when (forCheck) {
+//            EnumCheck.HOME -> {//when selected home
+//
+//            }
+//            EnumCheck.POSTREQUEST -> {//postrequest
+//                binding.stylishImage.visibility = View.INVISIBLE
+//                binding.mainTitleText.visibility = View.VISIBLE
+//                binding.mainTitleText.text = "需求刊登"
+//            }
+//            EnumCheck.RANKINGLIST -> {//rankinglist
+//                binding.stylishImage.visibility = View.INVISIBLE
+//                binding.mainTitleText.visibility = View.VISIBLE
+//                binding.mainTitleText.text = "排行榜"
+//            }
+//            EnumCheck.PROFILE -> {//profile
+//                binding.stylishImage.visibility = View.INVISIBLE
+//                binding.mainTitleText.visibility = View.VISIBLE
+//                binding.mainTitleText.text = "個人"
+//            }
+//
+//        }
+//    }
+
 }
 
 //        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
---
+++ app/src/main/java/com/tom/helper/source/HelperRepository.kt
@@ -0,0 +1,15 @@
+package com.tom.helper.source
+
+
+
+interface HelperRepository {
+
+//    suspend fun login(id: String): Result<Author>
+
+//    suspend fun getArticles(): Result<List<Article>>
+//
+//    suspend fun publish(article: Article): Result<Boolean>
+//
+//    suspend fun delete(article: Article): Result<Boolean>
+
+}
