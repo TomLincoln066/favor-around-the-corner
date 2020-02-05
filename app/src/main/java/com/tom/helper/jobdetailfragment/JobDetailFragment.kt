7ef145c7992bac49c34337a6ef18b5cd1577ded3
Will Lo <darthhun54@gmail.com>
2020-02-05 13:55:57 +0800

    build proposal edit fragment & can nav from job detail to proposal edit fragment & app works ok now

--- app/src/main/java/com/tom/helper/jobdetailfragment/JobDetailFragment.kt
+++ app/src/main/java/com/tom/helper/jobdetailfragment/JobDetailFragment.kt
@@ -6,13 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+
 
 import com.tom.helper.R
+import com.tom.helper.databinding.FragmentJobDetailBinding
+import com.tom.helper.proposaleditfragment.ProposalEditFragmentDirections
+import com.tom.helper.source.Task
 
 /**
  * A simple [Fragment] subclass.
  */
 class JobDetailFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +28,22 @@ class JobDetailFragment : Fragment() {
     ): View? {
 
 
+
+        val binding = FragmentJobDetailBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        binding.buttonJobDetailSendProposal.setOnClickListener {
+
+            //requireArguments().get("task") as Task) : handle getting the task argument from fragment_job_detail * should try navArg<>() instead
+            findNavController().navigate(JobDetailFragmentDirections.actionGlobalProposalEditFragment(requireArguments().get("task") as Task))
+//            findNavController().navigate(JobDetailFragmentDirections.actionGlobalProposalEditFragment(navArgs<Task>()))
+
+        }
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_job_detail, container, false)
+//        return inflater.inflate(R.layout.fragment_job_detail, container, false)
+        return binding.root
     }
 
 
