ed5cd869f10e14d628b000b0ccb2be15edd49f93
Will Lo <darthhun54@gmail.com>
2020-02-18 14:49:25 +0800

    revise some UI layout & works fine

---
+++ app/src/main/java/com/tom/helper/source/ProposalProgressContent.kt
@@ -0,0 +1,20 @@
+package com.tom.helper.source
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class ProposalProgressContent(
+
+    var content: String = "",
+    val status: Int = -1,
+    val finished: Boolean = false,
+    var id: String = "",
+    var createdTime: Long = -1,
+    val userID: String = "",
+    val user: User? = null,
+    val userIDMakeTheItem: User? = null,
+    val userIDTakeTheCase: User? = null,
+    val proposalID: String = ""
+
+) : Parcelable
